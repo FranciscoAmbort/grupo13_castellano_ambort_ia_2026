c7787f81671f7cd6c4af7fb94076ec25696620e3
Matias <matiascastellano93@gmail.com>
2026-05-28 12:13:58 +0000

    Continua entrega 2

--- entrega2.py
+++ entrega2.py
@@ -35,7 +35,7 @@ def build_camp(camp_size, habs, generators, labs, deposits, airlocks, craters):
         dominios[hab]=dominio_hab #agrega al diccionario de habitaciones, los dominios que no estan al borde
     for air in variables_air:
         dominios[air]= dominio_air #agrega al diccionario de esclusas de aire, los dominios que estan al borde 
-    for var in variables_gen,variables_lab,variables_dep:
+    for var in variables_gen+variables_lab+variables_dep:
         dominios[var]= dominio_general_variables        
     # sin superposicion
     def diferentes(variables, values):
@@ -55,20 +55,43 @@ def build_camp(camp_size, habs, generators, labs, deposits, airlocks, craters):
     #aislamiento hab y generador
     for gen in variables_gen:
         for hab in variables_hab:
-            restricciones.append((gen,hab),verificar_adyacencia)
+            restricciones.append(((gen,hab),verificar_adyacencia))
     #aislamiento generadores
     for gen1, gen2 in combinations((variables_gen), 2):
         restricciones.append(
         ((gen1, gen2), verificar_adyacencia)
     )
     def cadena_suministro(variables,values):
-        val1,val2= values
+        val1= values[0]
+        val2= values[1:]
         f1,c1= val1
-        f2,c2= val2
-        if ((f1+1 == f2 or f1-1 == f2) and c1==c2) or ((c1+1==c2 or c1-1==c2) and f1==f2):
-            return True 
-        return False
+        for f2,c2 in val2:
+            if ((f1+1 == f2 or f1-1 == f2) and c1==c2) or ((c1+1==c2 or c1-1==c2) and f1==f2):
+                return True #cuando encuentra al menos 1 deposito adyacente, return True.
+        return False #no encontro ningun deposito adyacente al laboratorio
     #laboratorios adyacentes a deposito
     for lab in variables_lab:
-        for dep in variables_dep:
-            restricciones.append((lab,dep),cadena_suministro)
+        variables_evaluar= [lab] + variables_dep
+        variables_evaluar_tupla= tuple(variables_evaluar)
+        restricciones.append((variables_evaluar_tupla,cadena_suministro))
+    
+    def modulo_habitacion_libre(variables,values):
+        val1= values[0]
+        val2= values[1:]
+        f1,c1= val1
+        contador=0
+        adyacentes= ((f1+1,c1),(f1-1,c1),(f1,c1+1),(f1,c1-1))
+        for a in adyacentes:
+            if (a in crateres_sets):
+                contador+=1
+        for f2,c2 in val2:
+            if ((f1+1 == f2 or f1-1 == f2) and c1==c2) or ((c1+1==c2 or c1-1==c2) and f1==f2):
+                contador+=1 #si encuentra algo en la posicion adyacente, devuelve False
+        if contador>3: 
+            return False
+        return True 
+    
+    for hab in variables_hab:
+        variables_evaluar= [hab] + variables_dep + variables_air+ variables_gen+variables_lab
+        variables_evaluar_tupla= tuple(variables_evaluar)
+        restricciones.append((variables_evaluar_tupla,modulo_habitacion_libre))    
